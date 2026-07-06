1803eb681708cf3210e602c7257ab21fe4c537b4
irinakiseleva0 <irina.kiseleva@epita.fr>
2026-07-07 00:36:51 +0200

    Add ConversionHistory model to track user conversions

--- exchange_app/models.py
+++ exchange_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ConversionHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='conversions')
+    from_currency = models.CharField(max_length=10)
+    to_currency = models.CharField(max_length=10)
+    amount = models.FloatField()
+    converted_amount = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.user.username}: {self.amount} {self.from_currency} -> {self.converted_amount} {self.to_currency}"
